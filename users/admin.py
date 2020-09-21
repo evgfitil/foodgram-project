3d0b402d8b985c4d68203303c659754dba68a92b
Евгений Ахметзянов <eugenueakhmetzyanov@evgakhm-mbp.local>
2020-09-21 10:21:21 +0300

    Remove the commented code

--- users/admin.py
+++
@@ -1,9 +0,0 @@
-# from django.contrib import admin
-# from .models import User
-#
-#
-# class MyUserAdmin(admin.ModelAdmin):
-#     filter_horizontal = ('email', 'usrname')
-#
-#
-# admin.site.register(User, MyUserAdmin)
