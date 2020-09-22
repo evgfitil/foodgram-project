6c6464a3ae2df6eedc9882618a4946b67e34d441
Евгений Ахметзянов <eugenueakhmetzyanov@evgakhm-mbp.local>
2020-09-22 17:13:40 +0300

    Debug to false and SECRET_KEY to .env

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -22,10 +22,10 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dz*3%s-gqzt_4i#gf$_s+9%61drj&e-3h+g^ou6py_b!(dplaw'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
